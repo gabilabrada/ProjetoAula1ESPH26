6e031d80bf6207d29dbb9e9f6dbcd8e56dd18b82
Gabriela <glabradadev@gmail.com>
2026-03-24 11:44:54 -0300

    feat: Declarações de variáveis JS

---
+++ src/js/script.js
@@ -0,0 +1,51 @@
+// Declarações e Variáveis
+
+// var, let e const
+
+//var x = 10;
+//console.log(x);
+//var x = 20
+// console.log(x);
+
+// var - pode redeclarar e acessar fora do escopo
+// let - não pode redeclarar e não consegue acessar fora do escopo
+
+// if(true){
+    //var nome = "Fiap";
+//}
+
+// console.log(nome)
+
+// const 
+// const taxaJuros = 0.85;
+// taxaJuros = 0.50;
+// console.log(taxaJuros);
+
+var exemplo1 = "Olá dev var";
+console.log(exemplo1);
+
+let exemplo2 = "Olá dev let";
+console.log(exemplo2);
+
+const exemplo3 = "Olá dev const";
+console.log(typeof exemplo3);
+
+let exemplo4 = 20;
+console.log(exemplo4);
+
+let exemplo5 = true;
+console.log(exemplo5);
+
+let exemplo6 = {nome:"fiap"};
+console.log(typeof exemplo6);
+
+let exemplo7 = ['Java', 'Python', "C#"];
+console.log(exemplo7);
+console.log(typeof exemplo7);
+
+// variável indefinida
+let exemplo8;
+console.log(exemplo8);
+
+let exemplo9 = null;
+console.log(exemplo9);
